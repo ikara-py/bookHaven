8e0c604254c49b63fb46572ace595615a41cab03
ikara-py <ali16kara@gmail.com>
2026-03-14 17:28:13 +0000

    feat: Create categories and books migration tables

---
+++ database/migrations/2026_03_14_170824_create_books_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('seller_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('author_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->string('cover')->nullable();
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->decimal('original_price', 10, 2)->nullable();
+            $table->unsignedInteger('stock')->nullable(0);
+            $table->year('publication_year')->nullable();
+            $table->string('language')->default('en');
+            $table->unsignedInteger('page_count')->nullable();
+            $table->string('pdf_path')->nullable();
+            $table->float('rating')->default(0);
+            $table->unsignedInteger('views')->default(0);
+            $table->unsignedInteger('downloads')->default(0);
+            $table->enum('type',['physical', 'digital'])->default('physical');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
